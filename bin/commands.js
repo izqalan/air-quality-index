48d8b5361d61b0e44449754fbf37ab2355674cd5
izqalan <izqalan@gmail.com>
2019-09-18 01:39:28 +0800

    chalked

--- bin/commands.js
+++ bin/commands.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-'use strict'
+
 require = require('esm')(module /*, options */);
+require('../src/app').aqi(process.argv);
 
 const program = require('commander');
+
 
 // functions
 const {
@@ -13,16 +15,16 @@ program.version('1.0.0')
     .description('Air Quality Index');
 
 program
-    .command('find <location>')
+    .command('find [query]')
     .alias('f')
     .description('find a city/town')
     .action(location => search(location));
 
-program
-    .command('findme')
-    .alias('fm')
-    .description('find local air quality index')
-    .action(search('here'));
+// program
+//     .command('findme <location>')
+//     .alias('fm')
+//     .description('find local air quality index')
+//     .action(search('here'));
 
 program.on('command:*', function () {
     console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
--- src/function.js
+++ src/function.js
@@ -1,5 +1,6 @@
 var request = require('request');
 const fetch = require('node-fetch');
+const chalk = require('chalk');
 
 var TOKEN = '97f08b9a493b5fd785bbf88d5d78a0e1cea8f832';
 var URI = 'https://api.waqi.info/feed/'
@@ -8,7 +9,10 @@ var URI = 'https://api.waqi.info/feed/'
 module.exports.search = search;
 
 
-// need location, token
+const qualityTag = [' [good]', ' [moderate]', ' [unhealthy for sensitive groups]', ' [unhealthy]',
+    ' [very unhealthy]', ' [hazardous]'];
+
+// need location
 
 async function search(location){
 
@@ -19,7 +23,7 @@ async function search(location){
         .then(response => response.json())
         .then(json => {
             if(json.status == 'error'){
-                console.log('cannot find station');
+                console.log(chalk.red('error! ') + 'cannot find station' );
             }else{
                 getAqi(json);
                 getCity(json);
@@ -35,11 +39,32 @@ async function search(location){
 async function getAqi(search_obj) {
     // console.log('getAqi: '+search_obj);
     if (search_obj.status == 'error') {
-        console.log('cannot find station');
+        console.log(chalk.red('error! ') + 'cannot find station');
         return 0;
     }else{
         var val = search_obj.data.aqi;
-        console.log('Air Quality: '+ search_obj.data.aqi);
+        var aqival = parseInt(val);
+        
+        if(aqival <= 50){
+            console.log(chalk.dim('Air Quality: ')+ val + chalk.green(qualityTag[0]));
+        }
+        else if( aqival > 50 && aqival <= 100 ){
+            console.log(chalk.dim('Air Quality: ')+ val + chalk.yellow(qualityTag[1]));
+        }
+        else if(aqival > 100 && aqival <= 150){
+            console.log(chalk.dim('Air Quality: ') + val + chalk.rgb(255,179,71)(qualityTag[2]));
+        }
+        else if (aqival > 150 && aqival <= 200) {
+            console.log(chalk.dim('Air Quality: ')+ val + chalk.red(qualityTag[3]));
+        }
+        else if(aqival > 200 && aqival <= 300){
+            console.log(chalk.dim('Air Quality: ')+ val + chalk.purple(qualityTag[4]));
+        }
+        else if(aqival > 300){
+            console.log(chalk.dim('Air Quality: ')+ val + chalk.magenta(qualityTag[5]));
+        }
+
+        // console.log('Air Quality: '+ search_obj.data.aqi);
         return val;
     }
 
@@ -48,11 +73,11 @@ async function getAqi(search_obj) {
 async function getCity(search_obj){
 
     if (search_obj.status == 'error') {
-        console.log('cannot find station');
+        console.log(chalk.red('error! ') + 'cannot find station');
         return 0;
     }else{
         var val = search_obj.data.city.name;
-        console.log('city: '+ val);
+        console.log(chalk.dim('city: ') + val);
         return val;
     }
 
@@ -61,13 +86,13 @@ async function getCity(search_obj){
 async function getTime(search_obj){
 
     if (search_obj.status == 'error') {
-        console.log('cannot find station');
+        console.log(chalk.red('error! ') + 'cannot find station');
         return 0;
     }else{
         var time = search_obj.data.time.s;
         var timezone = search_obj.data.time.tz;
-        console.log('timezone: '+ timezone);
-        console.log('last updated on '+ time);
+        console.log(chalk.dim('timezone: ') + timezone);
+        console.log(chalk.green('last updated on '+ time));
         return time;
     }
 
